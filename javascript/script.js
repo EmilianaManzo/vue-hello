af9c0efe581d696fd09ad7b5a0cb2c091f79eeeb
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-11 15:41:05 +0100

    [add] bonus più volte

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,11 @@ createApp({
   data(){
     return{
       message: 'Hello Vue!',
-      imageurl:'https://wips.plug.it/cips/buonissimo.org/cms/2018/07/adobestock_69886713.jpeg'
+      imageurl:'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExejI3Y3V5ZGE3M2xzdnRsNW9yaHc3enJ5c2ZvNHhnNDBoNmp1NDdzciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sJWNLTclcvVmw/giphy.gif',
+      imageCat: 'assets/img/la-guida-del-gattino1.jpg',
+      mieDim:'piccola',
+      // per capire se funziona inserendolo con v-html ma con gia presenti nomi delle variabili o se serve mettere percordo url o delle cartelle
+      imagehtml: '<img :src="imageCat" class="piccola" alt="gattini">'
     }
   }
 }).mount('#app');
