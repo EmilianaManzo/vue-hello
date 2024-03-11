ac62aa385f19bf0cc3600e8d138ba42f5a7f198a
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-11 18:09:19 +0100

    [add] esercitazioni

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,46 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
+      name: 'Emy',
       message: 'Hello Vue!',
       imageurl:'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExejI3Y3V5ZGE3M2xzdnRsNW9yaHc3enJ5c2ZvNHhnNDBoNmp1NDdzciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sJWNLTclcvVmw/giphy.gif',
       imageCat: 'assets/img/la-guida-del-gattino1.jpg',
       mieDim:'piccola',
-      // per capire se funziona inserendolo con v-html ma con gia presenti nomi delle variabili o se serve mettere percordo url o delle cartelle
-      imagehtml: '<img :src="imageCat" class="piccola" alt="gattini">'
+      text: '',
+      adiosmex :'',
+      isRed : false,
+      displayDateClock : '00/00/0000 00:00:00'
     }
+
+  },
+
+  methods:{
+    printDateeClock(){
+      const d = new Date();
+      const day= d.getDay() < 10 ? '0'+d.getDay() : d.getDay() ;
+      const month= d.getMonth() < 10 ? '0'+d.getMonth() : d.getMonth() ; 
+      const year= d.getFullYear();
+      const h= d.getHours() < 10 ? '0'+d.getHours() : d.getHours() ;
+      const m= d.getMinutes() < 10 ? '0'+d.getMinutes() : d.getMinutes() ;
+      const s= d.getSeconds() < 10 ? '0'+d.getSeconds() : d.getSeconds() ;
+
+      this.displayDateClock = `${day}/ ${month}/ ${year}  ${h}: ${m}: ${s} `;
+    },
+
+    startDateeClock(){
+      setInterval(()=>{
+        this.printDateeClock()
+      },1000);
+    },
+
+    salutoCongedo (salutostring){
+      this.adiosmex = `${salutostring} ${this.name} ci si vede alla prossima!`
+    }
+  },
+
+  mounted(){
+    this.printDateeClock(),
+    this.startDateeClock()
   }
+
 }).mount('#app');
